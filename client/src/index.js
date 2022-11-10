240e2e4b617ce5250cccf40dbfdf926892e0d918
Aaron Sanchez <saarong12@gmail.com>
2022-11-10 16:17:41 -0600

    React app is set up

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+import App from './App'
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+
+const root = createRoot(document.getElementById('root'))
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
