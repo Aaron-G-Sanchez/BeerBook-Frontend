bb6f5426a9de6867c688eed2a926928acbe42604
Aaron Sanchez <saarong12@gmail.com>
2022-11-12 15:26:43 -0600

    logo added to login

--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -14,6 +14,11 @@ const Signin = ({ setUser }) => {
   return (
     <>
       <section className="login-description">
+        <img
+          className="logo"
+          src="../assets/images/beerbook_logo.svg"
+          alt="logo"
+        />
         <h1 className="description-header">BEERBOOK</h1>
         <p className="description-body">
           Welcome to BeerBook. Your one stop shop to remember your favorit
@@ -21,11 +26,14 @@ const Signin = ({ setUser }) => {
         </p>
       </section>
       <section className="login-form">
-        <h1 className="login-header">Sign In</h1>
+        <h2 className="login-header">Sign In</h2>
         <form className="login-body" onSubmit={onSubmit}>
-          <input type="text" placeholder="Email"></input>
-          <input type="password" placeholder="Password"></input>
-          <input type="checkbox"></input>
+          <input type="text" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <div className="age-consent">
+            <input type="checkbox" name="iAm21" />
+            <label for="iAm21">I am 21+</label>
+          </div>
           <button type="submit">Sign In</button>
         </form>
       </section>
