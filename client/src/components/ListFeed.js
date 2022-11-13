0b95abf5745d518517cc758612369184584d4ee5
Aaron Sanchez <saarong12@gmail.com>
2022-11-12 23:58:16 -0600

    Added styling to the beer list feed

--- client/src/components/ListFeed.js
+++ client/src/components/ListFeed.js
@@ -8,6 +8,7 @@ const ListFeed = ({ feed }) => {
           ? feed.data.map((list) => (
               <div key={list.id} className="list">
                 <p className="list-name">{list.name}</p>
+                <p>{list.User.name}</p>
               </div>
             ))
           : null}
