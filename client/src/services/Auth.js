6079ef8e2da0684c4934ea76761e7db1653e5d39
Aaron Sanchez <saarong12@gmail.com>
2022-11-14 08:41:02 -0600

    Updated map on list feed

--- client/src/services/Auth.js
+++ client/src/services/Auth.js
@@ -3,7 +3,7 @@ import Client from './api'
 export const SignInUser = async () => {
   {
     try {
-      const response = await Client.get('/user/id')
+      const response = await Client.get('/user/1')
       return response
     } catch (error) {
       throw error
