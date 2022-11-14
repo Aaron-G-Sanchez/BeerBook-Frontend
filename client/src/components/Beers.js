40b68d6acd47c4a753bde252e1e772aa3a55a48b
Aaron Sanchez <saarong12@gmail.com>
2022-11-14 10:45:07 -0600

    Fixed beer spacing on the beer list

--- client/src/components/Beers.js
+++ client/src/components/Beers.js
@@ -3,9 +3,21 @@ const Beers = ({ beer }) => {
     <>
       <section className="beer-list">
         <h1>Beer</h1>
-        {beer
-          ? beer.data.map((beer) => <div key={beer.id}>{beer.name}</div>)
-          : null}
+        <div className="beer-wrapper">
+          {beer
+            ? beer.data.map((beer) => (
+                <div key={beer.id} className="single-beer">
+                  <h3>{beer.name}</h3>
+                  <p>{beer.brewery}</p>
+                  {/* <img
+                    className="beer-image"
+                    src={beer.image}
+                    alt="picture-of-beer"
+                  /> */}
+                </div>
+              ))
+            : null}
+        </div>
       </section>
     </>
   )
